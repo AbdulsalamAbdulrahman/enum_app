2d5740d1892f387a0870a455624ea8f9c47d09da
Abdulsalam Abdulrahman <asquare747@gmail.com>
2023-02-28 12:44:33 +0100

    fixed null error

--- lib/search.dart
+++ lib/search.dart
@@ -108,7 +108,8 @@ class _SearchState extends State<Search> {
                                               phone: dataList1[index]['phone'],
                                               mail: dataList1[index]['mail'],
                                               role: dataList1[index]['role'],
-                                              nin: dataList1[index]['nin'],
+                                              nin: dataList1[index]['nin'] ??
+                                                  "NA",
                                               id: widget.id,
                                               firstname: widget.firstname,
                                               lastname: widget.lastname,
@@ -121,14 +122,20 @@ class _SearchState extends State<Search> {
                                     fontSize: 24, color: Colors.white)),
                             title: Text('${dataList1[index]["fname"]}',
                                 style: const TextStyle(color: Colors.white)),
-                            subtitle: Text(
-                                dataList1[index]['mail'] +
-                                        "\n" +
-                                        dataList1[index]['phone'] +
-                                        "\n" +
-                                        dataList1[index]['nin'] ??
-                                    "NA",
-                                style: const TextStyle(color: Colors.white))),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(dataList1[index]['mail'] ?? "NA",
+                                    style:
+                                        const TextStyle(color: Colors.white)),
+                                Text(dataList1[index]['phone'] ?? "NA",
+                                    style:
+                                        const TextStyle(color: Colors.white)),
+                                Text(dataList1[index]['nin'] ?? "NA",
+                                    style:
+                                        const TextStyle(color: Colors.white)),
+                              ],
+                            )),
                       ),
                     )
                   : const Text(
